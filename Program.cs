0e27dad68064c0c9373bfa5bcd3b942daf99ea9a
danilo <danilo.cvijetic10@gmail.com>
2023-03-10 15:38:10 +0100

    Second commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿class Program{
 
     static void Main(string[] args){
+        var txt = Console.ReadLine();
         string[] data = Calculator.getInputData();
         int res = Calculator.Calculate(data);
         Console.WriteLine(res);
