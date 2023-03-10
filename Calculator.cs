bb025e83565aabaef52b4fc632f47c53c13f90f8
danilo <danilo.cvijetic10@gmail.com>
2023-03-10 15:35:20 +0100

    Initial commit!

---
+++ Calculator.cs
@@ -0,0 +1,31 @@
+class Calculator{
+
+    public static string[] getInputData(){
+        var input = Console.ReadLine();
+        while (input == null || input == ""){
+            Console.WriteLine("Greska u unosu!");
+            input = Console.ReadLine();
+        }
+        return input.Split(' ');
+    }
+
+    public static int Calculate(string[] data){
+        int num1 = Convert.ToInt32(data[0]);
+        int num2 = Convert.ToInt32(data[2]);
+
+        switch (data[1]){
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            case "*":
+                return num1 * num2;
+            case "/":
+                return num1 / num2;
+            default:
+                return -1;
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿class Program{
+
+    static void Main(string[] args){
+        string[] data = Calculator.getInputData();
+        int res = Calculator.Calculate(data);
+        Console.WriteLine(res);
+    }
+
+}
+
